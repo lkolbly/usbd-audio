e40751a67e382fc704852d30474048aec843c1d5
Lane Kolbly <lane@rscheme.org>
2021-04-18 19:08:21 -0500

    Cargo format

--- examples/teensy4/src/audio.rs
+++ examples/teensy4/src/audio.rs
@@ -9,24 +9,23 @@
 
 #![no_std]
 #![no_main]
-
 #![feature(alloc_error_handler)]
 
 #[macro_use]
 extern crate alloc;
 
-use support::bsp::t41;
-use support::hal;
-use support::ral;
-use support::hal::adc;
+use cortex_m_rt::interrupt;
 use embedded_hal::adc::OneShot;
 use support::bsp::interrupt;
-use cortex_m_rt::interrupt;
+use support::bsp::t41;
+use support::hal;
+use support::hal::adc;
+use support::ral;
 
-use usb_device::prelude::*;
-use usb_device::class_prelude::*;
 use alloc_cortex_m::CortexMHeap;
 use micromath::F32Ext;
+use usb_device::class_prelude::*;
+use usb_device::prelude::*;
 use usbd_audio::UsbAudio;
 
 const UART_BAUD: u32 = 115_200;
@@ -35,7 +34,7 @@ const BLINK_PERIOD: core::time::Duration = core::time::Duration::from_millis(500
 
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
-static mut HEAP: [u8; 128*1024] = [0; 128*1024];
+static mut HEAP: [u8; 128 * 1024] = [0; 128 * 1024];
 
 #[alloc_error_handler]
 fn handle_alloc_error(allocation: core::alloc::Layout) -> ! {
@@ -83,7 +82,58 @@ struct PolyphaseFilterBank {
     raw_samples: [f32; 50],
 }
 
-const FILTER: [f32; 50] = [-1.5571565755041362e-20,5.299655672641262e-06,3.729570824646748e-05,0.00012457763600283632,0.00030323871904505245,0.0006182758531686576,0.0011238702437696656,0.0018821463770603017,0.0029601834785402945,0.004425282087467552,0.006338740597132057,0.008748640145754124,0.011682338016292702,0.015139500358883382,0.019086542908262744,0.02345328251739534,0.028132434417186106,0.03298233459649817,0.03783294946319836,0.04249489045854946,0.046770818593343866,0.050468341909227984,0.0534133117654359,0.055462336662509264,0.05651336783135692,0.05651336783135692,0.05546233666250927,0.05341331176543591,0.050468341909227984,0.04677081859334388,0.04249489045854947,0.03783294946319837,0.032982334596498186,0.028132434417186106,0.023453282517395355,0.019086542908262744,0.015139500358883395,0.011682338016292707,0.008748640145754133,0.006338740597132062,0.00442528208746756,0.002960183478540299,0.0018821463770603017,0.0011238702437696673,0.0006182758531686576,0.0003032387190450536,0.0001245776360028364,3.7295708246467734e-05,5.299655672641164e-06,-1.5571565755041362e-20];
+const FILTER: [f32; 50] = [
+    -1.5571565755041362e-20,
+    5.299655672641262e-06,
+    3.729570824646748e-05,
+    0.00012457763600283632,
+    0.00030323871904505245,
+    0.0006182758531686576,
+    0.0011238702437696656,
+    0.0018821463770603017,
+    0.0029601834785402945,
+    0.004425282087467552,
+    0.006338740597132057,
+    0.008748640145754124,
+    0.011682338016292702,
+    0.015139500358883382,
+    0.019086542908262744,
+    0.02345328251739534,
+    0.028132434417186106,
+    0.03298233459649817,
+    0.03783294946319836,
+    0.04249489045854946,
+    0.046770818593343866,
+    0.050468341909227984,
+    0.0534133117654359,
+    0.055462336662509264,
+    0.05651336783135692,
+    0.05651336783135692,
+    0.05546233666250927,
+    0.05341331176543591,
+    0.050468341909227984,
+    0.04677081859334388,
+    0.04249489045854947,
+    0.03783294946319837,
+    0.032982334596498186,
+    0.028132434417186106,
+    0.023453282517395355,
+    0.019086542908262744,
+    0.015139500358883395,
+    0.011682338016292707,
+    0.008748640145754133,
+    0.006338740597132062,
+    0.00442528208746756,
+    0.002960183478540299,
+    0.0018821463770603017,
+    0.0011238702437696673,
+    0.0006182758531686576,
+    0.0003032387190450536,
+    0.0001245776360028364,
+    3.7295708246467734e-05,
+    5.299655672641164e-06,
+    -1.5571565755041362e-20,
+];
 
 impl PolyphaseFilterBank {
     fn consume(&mut self, data: &[f32]) -> alloc::vec::Vec<f32> {
@@ -110,21 +160,51 @@ impl PolyphaseFilterBank {
     fn compute(&self, I: &[f32; 21]) -> [f32; 8] {
         let F = &FILTER;
         let mut O = [0.0; 8];
-        O[0] = I[0] * F[7] + I[1] * F[15] + I[2] * F[23] + I[3] * F[31] + I[4] * F[39] + I[5] * F[47];
-        O[1] = I[2] * F[6] + I[3] * F[14] + I[4] * F[22] + I[5] * F[30] + I[6] * F[38] + I[7] * F[46];
-        O[2] = I[4] * F[5] + I[5] * F[13] + I[6] * F[21] + I[7] * F[29] + I[8] * F[37] + I[9] * F[45];
-        O[3] = I[6] * F[4] + I[7] * F[12] + I[8] * F[20] + I[9] * F[28] + I[10] * F[36] + I[11] * F[44];
-        O[4] = I[8] * F[3] + I[9] * F[11] + I[10] * F[19] + I[11] * F[27] + I[12] * F[35] + I[13] * F[43];
-        O[5] = I[10] * F[2] + I[11] * F[10] + I[12] * F[18] + I[13] * F[26] + I[14] * F[34] + I[15] * F[42];
-        O[6] = I[12] * F[1] + I[13] * F[9] + I[14] * F[17] + I[15] * F[25] + I[16] * F[33] + I[17] * F[41] + I[18] * F[49];
-        O[7] = I[14] * F[0] + I[15] * F[8] + I[16] * F[16] + I[17] * F[24] + I[18] * F[32] + I[19] * F[40] + I[20] * F[48];
+        O[0] =
+            I[0] * F[7] + I[1] * F[15] + I[2] * F[23] + I[3] * F[31] + I[4] * F[39] + I[5] * F[47];
+        O[1] =
+            I[2] * F[6] + I[3] * F[14] + I[4] * F[22] + I[5] * F[30] + I[6] * F[38] + I[7] * F[46];
+        O[2] =
+            I[4] * F[5] + I[5] * F[13] + I[6] * F[21] + I[7] * F[29] + I[8] * F[37] + I[9] * F[45];
+        O[3] = I[6] * F[4]
+            + I[7] * F[12]
+            + I[8] * F[20]
+            + I[9] * F[28]
+            + I[10] * F[36]
+            + I[11] * F[44];
+        O[4] = I[8] * F[3]
+            + I[9] * F[11]
+            + I[10] * F[19]
+            + I[11] * F[27]
+            + I[12] * F[35]
+            + I[13] * F[43];
+        O[5] = I[10] * F[2]
+            + I[11] * F[10]
+            + I[12] * F[18]
+            + I[13] * F[26]
+            + I[14] * F[34]
+            + I[15] * F[42];
+        O[6] = I[12] * F[1]
+            + I[13] * F[9]
+            + I[14] * F[17]
+            + I[15] * F[25]
+            + I[16] * F[33]
+            + I[17] * F[41]
+            + I[18] * F[49];
+        O[7] = I[14] * F[0]
+            + I[15] * F[8]
+            + I[16] * F[16]
+            + I[17] * F[24]
+            + I[18] * F[32]
+            + I[19] * F[40]
+            + I[20] * F[48];
         O
     }
 }
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    unsafe { ALLOCATOR.init(HEAP.as_ptr() as usize, 128*1024) }
+    unsafe { ALLOCATOR.init(HEAP.as_ptr() as usize, 128 * 1024) }
 
     let hal::Peripherals {
         iomuxc,
@@ -143,9 +223,9 @@ fn main() -> ! {
     let mut led = support::configure_led(pins.p13);
 
     // Timer for blinking
-    let (_, ipg_hz) =
-        ccm.pll1
-            .set_arm_clock(hal::ccm::PLL1::ARM_HZ, &mut ccm.handle, &mut dcdc);
+    let (_, ipg_hz) = ccm
+        .pll1
+        .set_arm_clock(hal::ccm::PLL1::ARM_HZ, &mut ccm.handle, &mut dcdc);
 
     let mut cfg = ccm.perclk.configure(
         &mut ccm.handle,
@@ -182,19 +262,24 @@ fn main() -> ! {
     let uart = uarts.uart6.init(pins.p1, pins.p0, UART_BAUD).unwrap();
 
     let (tx, _) = uart.split();
-    imxrt_uart_log::dma::init(tx, channel, imxrt_uart_log::LoggingConfig {
-        max_level: log::LevelFilter::Debug,
-        filters: &[
-        ],
-    }).unwrap();
+    imxrt_uart_log::dma::init(
+        tx,
+        channel,
+        imxrt_uart_log::LoggingConfig {
+            max_level: log::LevelFilter::Debug,
+            filters: &[],
+        },
+    )
+    .unwrap();
 
     // Setup the SAI mclk
     // Copied from https://github.com/PaulStoffregen/Audio/blob/master/output_i2s.cpp
     let fs = 44_100; //AUDIO_SAMPLE_RATE_EXACT
-    // PLL between 27*24 = 648MHz und 54*24=1296MHz
+                     // PLL between 27*24 = 648MHz und 54*24=1296MHz
     let n1 = 4; //SAI prescaler 4 => (n1*n2) = multiple of 4
     let n2 = 1 + (24000000 * 27) / (fs * 256 * n1);
-    ccm.pll4.set(&mut ccm.handle, fs as f64 * 256. * n1 as f64 * n2 as f64);
+    ccm.pll4
+        .set(&mut ccm.handle, fs as f64 * 256. * n1 as f64 * n2 as f64);
     unsafe {
         let iomuxc_gpr = hal::ral::iomuxc_gpr::IOMUXC_GPR::steal();
         hal::ral::modify_reg!(hal::ral::iomuxc_gpr, iomuxc_gpr, GPR1,
@@ -237,7 +322,13 @@ fn main() -> ! {
     let mut channel = dma_channels[8].take().unwrap();
     channel.set_interrupt_on_completion(true);
     unsafe {
-        streaming_adc = Some(hal::adc::StreamingAdc::new(adc1, a1, channel, &ADC_BUFFER, &ADC_BUFFER2));
+        streaming_adc = Some(hal::adc::StreamingAdc::new(
+            adc1,
+            a1,
+            channel,
+            &ADC_BUFFER,
+            &ADC_BUFFER2,
+        ));
     }
 
     unsafe {
@@ -283,7 +374,7 @@ fn main() -> ! {
         streaming_adc.as_mut().unwrap().start();
     }
 
-    let mut pfb = PolyphaseFilterBank{
+    let mut pfb = PolyphaseFilterBank {
         sample_offset: 0,
         nsamps: 0,
         raw_samples: [0.0; 50],
@@ -292,7 +383,10 @@ fn main() -> ! {
     let mut n_sent = 1;
 
     log::info!("SAI version: {:?}", sai1.version());
-    log::info!("0x{:x}", hal::ral::read_reg!(hal::ral::sai, sai1.regs(), TCSR));
+    log::info!(
+        "0x{:x}",
+        hal::ral::read_reg!(hal::ral::sai, sai1.regs(), TCSR)
+    );
 
     let mut last_sent_bytes = 0;
     let mut loops = 0;
@@ -305,7 +399,7 @@ fn main() -> ! {
     let mut nsamps = 0;
     let mut filtered_samps = 0;
     let mut last_processing_time = 0;
-    let mut mic_data = vec!();
+    let mut mic_data = vec![];
     let mut blocks = 0;
     let mut samps_dropped = 0;
     let mut i2s_samps_sent = 0;
@@ -362,7 +456,10 @@ fn main() -> ! {
                 "{} RX'd = {} TX'd = {} blocks = {} dropped = {} loops = {} proctime = {} nsamps = {} filtered_samps = {} min/max={}/{} filtered range={}/{} I2S samps sent = {} underruns = {} overruns = {} max = {}",
                 gpt2.count(), audio.nbytes, audio.nbytes_sent, blocks, samps_dropped, loops, last_processing_time, nsamps, filtered_samps, adc_min, adc_max, filtered_adc_min, filtered_adc_max, i2s_samps_sent, i2s_underruns, audio.usb_overruns, max_out_sample,
             );
-            log::info!("0x{:x}", hal::ral::read_reg!(hal::ral::sai, sai1.regs(), TCSR));
+            log::info!(
+                "0x{:x}",
+                hal::ral::read_reg!(hal::ral::sai, sai1.regs(), TCSR)
+            );
             nsamps = 0;
             filtered_samps = 0;
             filtered_adc_min = 0.0;
@@ -385,15 +482,19 @@ fn main() -> ! {
         }
 
         if mic_data.len() > 45 {
-            let end = if mic_data.len() > 45 { 45 } else { mic_data.len() };
+            let end = if mic_data.len() > 45 {
+                45
+            } else {
+                mic_data.len()
+            };
             match audio.source_ep.write(&mic_data[..end]) {
                 Ok(_) => {
                     mic_data = mic_data[end..].to_vec();
-                },
+                }
                 Err(usb_device::UsbError::WouldBlock) => {
                     // Would block, do nothing
                     blocks += 1;
-                },
+                }
                 Err(e) => {
                     panic!("Endpoint send error {:?}", e);
                 }
