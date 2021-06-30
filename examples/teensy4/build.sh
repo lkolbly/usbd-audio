2473f2f375cb73438ae6b51918157971d05d2ebb
Lane Kolbly <lane@rscheme.org>
2021-06-30 18:29:45 -0500

    Add submodules

--- examples/teensy4/build.sh
+++ examples/teensy4/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-pushd ../../
-cargo +nightly test
-popd
+#pushd ../../
+#cargo +nightly test
+#popd
 
 cargo +nightly build --release
 arm-none-eabi-objcopy target/thumbv7em-none-eabihf/release/audio -O ihex target/thumbv7em-none-eabihf/release/audio.hex
